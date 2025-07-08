06a12f817c62b21b7b8bf9739e59dc0a7ecddf41
Noah <science103555@gmail.com>
2025-07-08 10:50:20 +0800

    update

--- output/regfile_map.h
+++ output/regfile_map.h
@@ -10,14 +10,14 @@
 #define CTRL_HW_INTR_DEST      (0x1 << 20)
 
 // autogen_dest_start
-#define CSR_DEST               (0x1 <<  0)
-#define SDMA_DEST               (0x1 <<  1)
-#define LDMA_DEST               (0x1 <<  2)
-#define FME0_DEST               (0x1 <<  3)
-#define RESERVED_4_DEST               (0x1 <<  4)
-#define RESERVED_5_DEST               (0x1 <<  5)
-#define LDMA2_DEST               (0x1 <<  6)
-#define CDMA_DEST               (0x1 <<  7)
+#define CSR_DEST        (0x1 << 0)
+#define SDMA_DEST       (0x1 << 1)
+#define LDMA_DEST       (0x1 << 2)
+#define FME0_DEST       (0x1 << 3)
+#define RESERVED_4_DEST (0x1 << 4)
+#define RESERVED_5_DEST (0x1 << 5)
+#define LDMA2_DEST      (0x1 << 6)
+#define CDMA_DEST       (0x1 << 7)
 // autogen_dest_stop
 
 
